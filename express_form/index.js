8121f255a1b345152a072a4b44ebd1ec1aee876e
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-15 12:31:06 -0700

    add a request handler to accept a post

--- express_form/index.js
+++ express_form/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 const app = express()
+app.use(express.urlencoded({extended: true})) //tell to express that the data will be enconded in the url
 
 app.get('/', (req,res) => {
     const page = `
@@ -13,7 +14,16 @@ app.get('/', (req,res) => {
             </body>
         </html>
         ` //this will be the page when someone enter to "/"
+        //when submit will try to post that infomration in "/person"
     res.send(page) //here we tell to responde with the page
+})
+
+app.post('/person', (req,res) => {
+    console.log('new person recived');
+
+    const name = req.body.name //will obtain the name from the request body from the input named "name"
+    res.send(name) //send the response to the url /person
+    console.log(name); //just send to the terminal
 })
 
 app.listen(3000, () => console.log('Server ready')) //upload our server in localhost:3000
