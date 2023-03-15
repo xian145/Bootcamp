297f61a91054864a831365a4b19981101a05bf5a
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-15 12:46:43 -0700

    add some comments

--- express_form/index.js
+++ express_form/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 const app = express()
 app.use(express.urlencoded({extended: true})) //tell to express that the data will be enconded in the url
+const people = [
+    { name: 'Flavio' },
+    { name: 'Roger' }
+    ]
 
 app.get('/', (req,res) => {
     const page = `
@@ -11,10 +15,15 @@ app.get('/', (req,res) => {
                     <input type="text" name="name" />
                     <input type="submit">
                 </form>
+                <h2>List of people:</h2>
+                <ul>
+                    ${people.map(person => `<li>${person.name}</li>`).join('')}
+                </ul>
             </body>
         </html>
         ` //this will be the page when someone enter to "/"
         //when submit will try to post that infomration in "/person"
+        //poeple.map will go through the array and create a list from each person as a string (thats why we use .join(''))
     res.send(page) //here we tell to responde with the page
 })
 
