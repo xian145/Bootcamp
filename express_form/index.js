1e484c98c41c7cd3464a95929c04d6b89c29b73e
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-15 12:09:40 -0700

    render a simple form in

--- express_form/index.js
+++ express_form/index.js
@@ -1,7 +1,19 @@
 import express from "express"
 const app = express()
 
-app.get('/', (req,res) => 
-res.send('hello world'))
+app.get('/', (req,res) => {
+    const page = `
+        <html>
+            <body>
+                <h1>Enter a new person:</h1>
+                <form action="/person" method="POST">
+                    <input type="text" name="name" />
+                    <input type="submit">
+                </form>
+            </body>
+        </html>
+        ` //this will be the page when someone enter to "/"
+    res.send(page) //here we tell to responde with the page
+})
 
 app.listen(3000, () => console.log('Server ready'))
