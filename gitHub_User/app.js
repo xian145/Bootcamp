78b33b94707e183af0a85e9b608fb782dcbcd937
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-14 18:50:07 -0700

    add selector and a listener for an event

--- gitHub_User/app.js
+++ gitHub_User/app.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    
+    const form = document.querySelector("form") //select the form
+    form.addEventListener("submit", (event) => { //add a event whe hear the submit
+        
+    })
 })
