17720c887399d3bf46884cb0eb3a238a645312f8
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-14 22:24:14 -0700

    add a funtion that fetch information from the github API and transform that information into a card, using a function which create a string tha we instert using instertAdjacentHTML function

--- gitHub_User/app.js
+++ gitHub_User/app.js
@@ -6,6 +6,59 @@ document.addEventListener("DOMContentLoaded", ()=> {
         const username = document.querySelector("input").value //this constant will get its value from the input of the form
         const response = await fetch(`https://api.github.com/users/${username}`) //this will fetch infromation from the API serching for the username we introduce from the form
         const data = await response.json() //once we have the response we convert that information into json.
-        console.log(data);
+
+        const createCard = (data) => `
+        <div class="px-4 py-5 sm:px-6 -ml-4 -mt-4 border-b border-gray-200 pb-8 flex justify-between items-center flex-wrap sm:flex-no-wrap">
+            <div class="ml-4 mt-4">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0">
+                        <img class="h-12 w-12 rounded-full" src="${data.avatar_url}" alt="User avatar" />
+                    </div>
+                    <div class="ml-4">
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            ${data.name}
+                            <span class="text-sm leading-5 text-gray-500">
+                                @${data.login}
+                            </span>
+                        </h3>
+                        <p class="text-sm leading-5 text-gray-500">
+                            ${
+                            data.public_repos
+                            } repositories. User since ${data.created_at.slice(0, 4)}
+                        </p>
+                        <p class="text-sm leading-5 text-gray-500">
+                            ${data.location || ""}
+                        </p>
+                        <p class="mt-1 text-sm leading-5 text-gray-500">
+                            ${data.bio}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <div class="ml-4 mt-4 flex-shrink-0 flex">
+        <span class="ml-3 inline-flex rounded-md shadow-sm">
+            <a href="${
+                data.html_url
+            }"><button type="button" class="mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-50 active:text-gray-800">
+            <span>
+                Profile
+            </span>
+            </button>
+            </a>
+            <a href="${
+                data.blog
+            }"><button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-50 active:text-gray-800">
+                <span>
+                    Website
+                </span>
+                </button>
+            </a>
+        </span>
+        </div>
+        </div>
+      ` //this function will create a string tha will be added to the HTML with all the tailwindcss style
+        //all that information is fetch from the API and we just give them form
+        const card = createCard(data) //call the function and introduce the "data"
+        document.querySelector('#container').insertAdjacentHTML("beforebegin", card) //on id=conatiner will add as a string in HTML what is contained in "card"
     })
 })
