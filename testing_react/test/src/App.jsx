acfd1becb3a37b42501a37cd304b09e0459bdbc1
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-16 07:30:54 -0700

    erased the base and just add a little message as a title and comments

--- testing_react/test/src/App.jsx
+++ testing_react/test/src/App.jsx
@@ -2,31 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+function WelcomeMessage() {
+  return <h1>Welcome!</h1>
+} //we add a new function that will be a simple title h1 message (A component)
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <WelcomeMessage/> {/* this is the way we call the funtion we previously talk */}
     </div>
   )
 }
