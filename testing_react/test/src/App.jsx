4fdf09ad7681f4586d9eafb273582a288be68951
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-16 16:57:12 -0700

    comments on how it works react

--- testing_react/test/src/App.jsx
+++ testing_react/test/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg' //calling an image from the folder "assets"
 import './App.css'
 
 function WelcomeMessage() {
@@ -8,10 +8,14 @@ function WelcomeMessage() {
 
 function App() {
   const [count, setCount] = useState(0)
+  const message = 'Guten Tag' //we can make constants and use them in the return to print them in the page
 
   return (
     <div className="App">
       <WelcomeMessage/> {/* this is the way we call the funtion we previously talk */}
+      <img src={reactLogo} className="logo react"></img> {/* we add the image, using the import image from the begining using curly brackets */}
+      <h2>{message === 'Hello!' ? 'The message was hello!' : message}</h2> {/* as we said you can use JS inside of the HTML */}
+      <button onClick={(event) => alert("YOU PRESS THE BUTTON!!!!!!!!!!!!")}>Don't press it</button> {/* we add an on click event and what to do with that in the same code */}
     </div>
   )
 }
