052db5c30d2ba84630c68cb63586983a345d83fb
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-18 16:57:05 -0700

    modified code to create a simple counter for clicks using useState

--- testing_react/test/src/App.jsx
+++ testing_react/test/src/App.jsx
@@ -11,7 +11,9 @@ const buttonPress = (event) => { //here, i create a new constan that will be cal
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) // they dont need to be called with that name, just an example
+  //first is the counter, just as we create a constant and the second is a function that modifieds "count"
+
   const message = 'Guten Tag' //we can make constants and use them in the return to print them in the page
 
   return (
@@ -21,6 +23,11 @@ function App() {
       <h2>{message === 'Hello!' ? 'The message was hello!' : message}</h2> {/* as we said you can use JS inside of the HTML */}
       <button onClick={(event) => alert("YOU PRESS THE BUTTON!!!!!!!!!!!!")}>Don't press it</button> {/* we add an on click event and what to do with that in the same code */}
       <button onClick={buttonPress}>Press it</button> {/* we can define the event handler o function outside and call it here */}
+      <div> {/* A div which contains buttons that modified a counter and a buttos which reset that cunter. */}
+        <p>You clicked {count} time the button clicks</p> {/* wher we can llok for the counter */}
+        <button onClick={() => setCount(count + 1)}>Clicks</button> {/* button with the actual funcion to modified the "count" */}
+        <button onClick={() => setCount(0)}>Reset</button> {/* another button to set count to cero */}
+      </div>
     </div>
   )
 }
