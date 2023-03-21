88c781c86764fcf9c34d0483239b041e178982c7
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-20 21:41:46 -0700

    give to the other buttons the same property and to modified the cpunter and create a button which will set the cunter to cero when clicked

--- testing_react/Demo-1-Counter/src/App.jsx
+++ testing_react/Demo-1-Counter/src/App.jsx
@@ -5,16 +5,18 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
   const increment = (step) => {
-    setCount(count + step)
+    if (typeof step === 'string') {
+      setCount(0)
+    } else setCount(count + step)
   } // this function us passed to the chindren and call it when the button is pressed
 
   return (
     <div className='App'>
       <h1>Counter: {count}</h1>
       <Button step={1} increment={increment}/> {/* one is the name of the prop and the other is the name of the function */}
-      <Button step={10}/>
-      <Button step={100}/>
-      <Button step={'Clear'}/> {/* every one of this components send a prop named step and increment */}
+      <Button step={10} increment={increment}/>
+      <Button step={100} increment={increment}/>
+      <Button step={'Clear'} increment={increment}/> {/* every one of this components send a prop named step and increment */}
     </div>
   )
 }
