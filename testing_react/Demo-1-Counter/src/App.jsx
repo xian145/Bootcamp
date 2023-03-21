83ba1ee73f01bbca903665c2b0abdffdf8c6b9f0
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-20 21:52:09 -0700

    add some otyher functions like other cpunter which save the last value of count and add de las value of count, something like calculator but simpler

--- testing_react/Demo-1-Counter/src/App.jsx
+++ testing_react/Demo-1-Counter/src/App.jsx
@@ -4,9 +4,15 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [counter, setCounter] =useState(0)
   const increment = (step) => {
-    if (typeof step === 'string') {
-      setCount(0)
+    if (step === 'Clear') {
+      setCount(0) 
+      setCounter(0)
+      return
+    }if (step === 'Save') {
+      setCounter(counter + count)
+      return
     } else setCount(count + step)
   } // this function us passed to the chindren and call it when the button is pressed
 
@@ -16,7 +22,12 @@ function App() {
       <Button step={1} increment={increment}/> {/* one is the name of the prop and the other is the name of the function */}
       <Button step={10} increment={increment}/>
       <Button step={100} increment={increment}/>
+      <Button step={-1} increment={increment}/>
+      <Button step={-10} increment={increment}/>
+      <Button step={-100} increment={increment}/>
+      <Button step={'Save'} increment={increment}/>
       <Button step={'Clear'} increment={increment}/> {/* every one of this components send a prop named step and increment */}
+      <p>{counter}</p>
     </div>
   )
 }
