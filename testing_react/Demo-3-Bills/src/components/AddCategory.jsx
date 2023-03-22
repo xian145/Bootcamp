684f97efadcfb411c373b1322a1693c123941613
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 22:49:24 -0700

    add some comments and a function to handle the submit of the form, at the moment is just an alert if the inputs is empty and if not call a function that was pass from the parent as a prop that is not defined yet

--- testing_react/Demo-3-Bills/src/components/AddCategory.jsx
+++ testing_react/Demo-3-Bills/src/components/AddCategory.jsx
@@ -5,11 +5,22 @@ function AddCategory(props) {
 
   const handleChange = (event) => { //event can had any name
     setCategory(event.target.value)
-    console.log(category)
+    // console.log(category) //this is only to know if the code is working
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault() //when the event is fired prevent reload the page
+
+    if (!category) { //this will fire if there is no a string
+      alert('Please enter a category')
+      return
+    }
+
+    props.onSubmit(category)
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}> {/* function called when we submit the form */}
       <h1>Enter a category of bills</h1>
       <input 
       value={category} //need to be value
@@ -17,7 +28,7 @@ function AddCategory(props) {
       className='form-input block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
       placeholder="Internet, Gas, etc"/>
       <button className='ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 
-      focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800'>Add</button>
+      focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800'>Add</button> {/* need to add a function for summit event */}
     </form>
   )
 }
