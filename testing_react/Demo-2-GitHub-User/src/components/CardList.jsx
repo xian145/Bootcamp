bfce0898516fcc31a767ea21b52dd1bf5f545b11
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 18:29:27 -0700

    add more comments

--- testing_react/Demo-2-GitHub-User/src/components/CardList.jsx
+++ testing_react/Demo-2-GitHub-User/src/components/CardList.jsx
@@ -7,7 +7,7 @@ const CardList = (props) => {
             <Card key={index} {...card} />
           ))} {/* prop.cards.map gonna run for every component in the array cards (that is actually data with json) and create a card sending prop key which is the unique 
           index created with map starting from 0 and the data in form of json */}
-          {/* this gonna call Card */}
+          {/* this gonna call Card with prop key = index and card json data*/}
           {/* cards is a prop sended from the app */}
         </div>
     )
