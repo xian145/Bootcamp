a52f470d769bf19f209afb5ac0986a48fc625382
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-22 11:59:58 -0700

    im not sure why dont work

--- testing_react/Demo-3-Bills/src/App.jsx
+++ testing_react/Demo-3-Bills/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     
     if (!categoriesInLocalStorage) { //if there is nothing here, change to true
       setShouldShowAddCategory(true) //change to true if it is empty so we render AddCategories
+      console.log(categoriesInLocalStorage);
     } //this will run over the useState defined first
   }, []) //this empty array will make to run this only one time when the DOM is mounted
 
