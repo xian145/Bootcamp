0358a8d7cfe3dd1cd4636e1606fa3f16efa357af
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 17:44:15 -0700

    more comments on how it works

--- testing_react/Demo-2-GitHub-User/src/App.jsx
+++ testing_react/Demo-2-GitHub-User/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'> {/* paddin top 10, text centered, margin top 6, text extra large, spaces between lines 9, font extra bold, text gray */}
         Search a GitHub User
       </h1>
-      <Form onSubmit={addNewCard} /> {/* on submit gonna call the function addNewCard */}
+      <Form onSubmit={addNewCard} /> {/* this is actually a prop function that is sended to the child (form) when submitted */}
       <CardList cards={cards} /> {/* cards is an array that will be passed as a prop to CardList */}
     </div>
   )
