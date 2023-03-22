ab9fdb6a28eff774150627c144cf44665c3e0974
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 23:00:13 -0700

    add a new function that will modified the array with al categories

--- testing_react/Demo-3-Bills/src/App.jsx
+++ testing_react/Demo-3-Bills/src/App.jsx
@@ -8,6 +8,10 @@ function App() {
   const [shouldShowAddCategory, setShouldShowAddCategory] = useState(true)
   const [categories, setCaries] = useState([]) //create a state modifier for categories which are an empty array
 
+  const addCategory = (category) => {
+
+  } //new function to add the categories to an array
+
   return (
     <div>
       {shouldShowAddCategory ? ( //look if thats true and if it is render the AddCategory component in the DOM
