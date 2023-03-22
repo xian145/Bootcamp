cfde2f915fb936935027bb9b4ae8d1d64bd9b0f8
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 17:24:06 -0700

    add a new funtion that will be adding eahc new card

--- testing_react/Demo-2-GitHub-User/src/App.jsx
+++ testing_react/Demo-2-GitHub-User/src/App.jsx
@@ -4,6 +4,11 @@ import CardList from "./components/CardList"
 
 function App() {
   const [cards, setCards] = useState([]) //instead of modified one card, we gonna create an array of cards
+  
+  const addNewCard = (cardInfo) => {
+    
+  }
+  
   return (
     <div>
       <p className="text-blue-500">Hello, World!</p>
