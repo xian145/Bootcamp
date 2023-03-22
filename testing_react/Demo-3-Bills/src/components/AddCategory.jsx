b45ec28e0d09f3ca8d7895519ea4a5a91ac50e1f
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-22 08:30:16 -0700

    used tailwind for the form

--- testing_react/Demo-3-Bills/src/components/AddCategory.jsx
+++ testing_react/Demo-3-Bills/src/components/AddCategory.jsx
@@ -20,15 +20,25 @@ function AddCategory(props) {
   }
 
   return (
-    <form onSubmit={handleSubmit}> {/* function called when we submit the form */}
-      <h1>Enter a category of bills</h1>
-      <input 
-      value={category} //need to be value
-      onChange={handleChange} //on any change gonna call handleChange function
-      className='form-input block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
-      placeholder="Internet, Gas, etc"/>
-      <button className='ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 
-      focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800'>Add</button> {/* need to add a function for summit event */}
+    <form
+      className='h-100 w-full flex items-center justify-center font-sans'
+      onSubmit={handleSubmit}>
+      <div className='bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg'>
+        <div className='mb-4'>
+          <h1 className='text-grey-darkest'>Enter a category of bills</h1>
+          <div className='flex mt-4'>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker'
+              placeholder='Electricity, Gas, etc'
+              value={category}
+              onChange={handleChange}
+            />
+            <button className='flex-no-shrink p-2 border-2 rounded bg-teal bg-green-500 text-white border-teal hover:text-white hover:bg-teal'>
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
     </form>
   )
 }
