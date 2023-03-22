6c6d310f3250a1e5d4f0c145522383445e0ff92b
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-22 08:04:27 -0700

    add new funtion addCategory to modified the value of the array with teh categories

--- testing_react/Demo-3-Bills/src/App.jsx
+++ testing_react/Demo-3-Bills/src/App.jsx
@@ -6,11 +6,13 @@ import NavBar from "./components/NavBar"
 
 function App() {
   const [shouldShowAddCategory, setShouldShowAddCategory] = useState(true)
-  const [categories, setCaries] = useState([]) //create a state modifier for categories which are an empty array
+  const [categories, setCategories] = useState([]) //create a state modifier for categories which are an empty array
 
-  const addCategory = (category) => {
-
-  } //new function to add the categories to an array
+  const addCategory = (category) => { //function which enter category as a value when called
+    const updateCategories = [...(categories || []), category] //const with the empty array that will be filled
+    setCategories(updateCategories) //function to modified the array with the new category
+    setShouldShowAddCategory(false) //change to false bc we already add a category and need to be replace we a new render for NavBar and BillsTable
+  } //new function to add the categories to the array
 
   return (
     <div>
