26f21907159809978b896ce7226a6a5781fc3d10
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 17:29:48 -0700

    add the form and the list of cards to the app.jsx

--- testing_react/Demo-2-GitHub-User/src/App.jsx
+++ testing_react/Demo-2-GitHub-User/src/App.jsx
@@ -6,12 +6,16 @@ function App() {
   const [cards, setCards] = useState([]) //instead of modified one card, we gonna create an array of cards
   
   const addNewCard = (cardInfo) => {
-    
+
   }
   
   return (
     <div>
-      <p className="text-blue-500">Hello, World!</p>
+      <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'> {/* paddin top 10, text centered, margin top 6, text extra large, spaces between lines 9, font extra bold, text gray */}
+        Search a GitHub User
+      </h1>
+      <Form onSubmit={addNewCard} /> {/* on submit gonna call the function addNewCard */}
+      <CardList cards={cards} /> {/* cards is an array that will be passed as a prop to CardList */}
     </div>
   )
 }
