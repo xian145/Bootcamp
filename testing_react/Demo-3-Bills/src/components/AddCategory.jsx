6bcacd5d85b80dae4e8cc15b9b2b901a414acde5
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 21:59:37 -0700

    add a simple form to add categories and some tailwind css

--- testing_react/Demo-3-Bills/src/components/AddCategory.jsx
+++ testing_react/Demo-3-Bills/src/components/AddCategory.jsx
@@ -1,5 +1,13 @@
 function AddCategory(props) {
-  return <div>AddCategory</div>
+  return (
+    <form>
+      <h1>Enter a category of bills</h1>
+      <input className='form-input block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
+      placeholder="Internet, Gas, etc"/>
+      <button className='ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 
+      focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800'>Add</button>
+    </form>
+  )
 }
 
 export default AddCategory
