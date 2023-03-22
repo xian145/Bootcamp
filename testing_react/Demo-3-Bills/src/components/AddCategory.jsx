01cce7d50399f4cf3c90ecc26eeffa17283b875d
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-21 22:38:15 -0700

    add the input value and a function called every time is a change in the input

--- testing_react/Demo-3-Bills/src/components/AddCategory.jsx
+++ testing_react/Demo-3-Bills/src/components/AddCategory.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react" //gonna change states
+
 function AddCategory(props) {
+  const [category, setCategory] = useState('')
+
+  const handleChange = (event) => { //event can had any name
+    setCategory(event.target.value)
+    console.log(category)
+  }
+
   return (
     <form>
       <h1>Enter a category of bills</h1>
-      <input className='form-input block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
+      <input 
+      value={category} //need to be value
+      onChange={handleChange} //on any change gonna call handleChange function
+      className='form-input block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
       placeholder="Internet, Gas, etc"/>
       <button className='ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 
       focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800'>Add</button>
