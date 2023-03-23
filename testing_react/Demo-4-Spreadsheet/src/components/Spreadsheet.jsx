f22506e7a96064ac24f2a01546e3339c0126b3b8
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-23 11:38:21 -0700

    add a prop named initialValue to the cel component

--- testing_react/Demo-4-Spreadsheet/src/components/Spreadsheet.jsx
+++ testing_react/Demo-4-Spreadsheet/src/components/Spreadsheet.jsx
@@ -17,8 +17,10 @@ export default function Spreadsheet() {
     <div className={`grid grid-cols-${data[0].length}`}> {/* js code returns the lenght of the first array in teh array */}
       {data.map((row, y) => { //data is the array of 8 arrays with 4 items each, with a total of 32 elements, so this will map each 32 elements
         // 'row' is the array inside of the array and 'y' is the index of the array inside the array
-        return row.map((cell, x) => { //run for every element inside the array for each array
-          return <Cell key={y + '-' + x} /> //render a div for eache element in the arrays, a total of 32, the key is the index inside the first array with the index of the second array (0-4)
+        return row.map((cell, x) => { //run for every element inside the array for each array, cell is the value inside of the array and x the index
+          return <Cell 
+          initialValue={cell} //value inside of the array passed as a props named initialValue
+          key={y + '-' + x} /> //render a div for eache element in the arrays, a total of 32, the key is the index inside the first array with the index of the second array (0-4)
         })
       })}
     </div>
