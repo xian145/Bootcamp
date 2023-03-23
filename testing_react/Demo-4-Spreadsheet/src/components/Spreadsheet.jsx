3e29e935bfc8ffe2ca36acac6494b5865e327840
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-22 22:43:30 -0700

    spreadsheet component will render a div using cell component for each element in the array of arrays, with a total of 32 divs

--- testing_react/Demo-4-Spreadsheet/src/components/Spreadsheet.jsx
+++ testing_react/Demo-4-Spreadsheet/src/components/Spreadsheet.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Cell from './Cell' //call cell from component cell
 
 export default function Spreadsheet() {
   const [data, setData] = useState([ //array containing 8 arrays
@@ -14,7 +15,11 @@ export default function Spreadsheet() {
 
   return (
     <div>
-      Spreadsheet
+      {data.map((row, y) => { //data is the array of 8 arrays with 4 items each, with a total of 32 elements, so this will map eache 32 elements
+        return row.map((cell, x) => {
+          return <Cell key={y + '-' + x} />
+        })
+      })}
     </div>
   )
 }
