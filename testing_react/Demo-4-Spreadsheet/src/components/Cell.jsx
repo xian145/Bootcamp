e8ee6624d4a7070b65970aa0e2658e89b2c1335c
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-23 09:25:40 -0700

    add a function to change true the state of isSelected, and when is true render a input in the div

--- testing_react/Demo-4-Spreadsheet/src/components/Cell.jsx
+++ testing_react/Demo-4-Spreadsheet/src/components/Cell.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+
 function Cell() {
-  return <div className='border-2 p-3'>cell</div> //only creat a div with the cell text
+  const [isSelected, setIsSelected] = useState(false)
+
+  const cellStyle = 'border-2 p-3'
+
+  if (isSelected) { //if its true will change the div to an input
+    return <input className={cellStyle}/>
+  }
+
+  return <div
+  onClick={(event) => {setIsSelected(true)}} //change to true if we click it
+  className={cellStyle}
+  >cell
+  </div> //only creat a div with the cell text
 }
 
 export default Cell
