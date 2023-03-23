9718fffe3e5bf0b74898cdcf4820f63169696e71
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-22 19:31:48 -0700

    add some style

--- testing_react/Demo-3-Bills/src/components/Addbill.jsx
+++ testing_react/Demo-3-Bills/src/components/Addbill.jsx
@@ -32,7 +32,7 @@ function AddBill(props) {
     <form
       className='h-100 w-full flex items-center justify-center font-sans'
       onSubmit={handleSubmit}>
-      <div className='bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg'>
+      <div className='bg-white rounded shadow-lg p-6 m-4 w-full lg:w-3/4 sm:max-w-lg'>
         <div className='mb-4'>
           <h1 className='text-grey-darkest'>Enter a new bill</h1>
           <div className='flex mt-4'>
@@ -43,7 +43,9 @@ function AddBill(props) {
               value={amount}
               onChange={handleChangeAmount}
             />
-            <select onChange={(event) => setCategory(event.target.value)}> {/* on change gonna put all the option bringed from categories */}
+            <select 
+            className='border border-solid border-gray-400 rounded-md hover:border-green-400 cursor-pointer'
+            onChange={(event) => setCategory(event.target.value)}> {/* on change gonna put all the option bringed from categories */}
               {props.categories
                 ? props.categories.map((value, index) => { //if we have a category, gonna map each value and return for each one as a option to pick
                     return (
@@ -55,7 +57,9 @@ function AddBill(props) {
                 : ''} {/* if there is no category on categories, just will show a blank space */}
             </select>
             <div className='mt-2 ml-1'>
-              <DatePicker selected={date} onChange={handleChangeDate} /> {/* this will show the date when we introduce the amount */}
+              <DatePicker 
+              className='border border-solid border-gray-400 rounded-md hover:border-green-400 cursor-pointer'
+              selected={date} onChange={handleChangeDate} /> {/* this will show the date when we introduce the amount */}
             </div>
             <button className='flex-no-shrink p-2 border-2 rounded bg-teal bg-purple-500 text-white border-teal hover:text-green-300 hover:bg-teal'>
               Add
