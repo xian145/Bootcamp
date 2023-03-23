902c2a2b14661ca762ba4ea25661c8660428ad12
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-23 07:45:25 -0700

    add comments on spreadsheet component

--- testing_react/Demo-4-Spreadsheet/src/components/Spreadsheet.jsx
+++ testing_react/Demo-4-Spreadsheet/src/components/Spreadsheet.jsx
@@ -15,9 +15,10 @@ export default function Spreadsheet() {
 
   return (
     <div>
-      {data.map((row, y) => { //data is the array of 8 arrays with 4 items each, with a total of 32 elements, so this will map eache 32 elements
-        return row.map((cell, x) => {
-          return <Cell key={y + '-' + x} />
+      {data.map((row, y) => { //data is the array of 8 arrays with 4 items each, with a total of 32 elements, so this will map each 32 elements
+        // 'row' is the array inside of the array and 'y' is the index of the array inside the array
+        return row.map((cell, x) => { //run for every element inside the array for each array
+          return <Cell key={y + '-' + x} /> //render a div for eache element in the arrays, a total of 32, the key is the index inside the first array with the index of the second array (0-4)
         })
       })}
     </div>
