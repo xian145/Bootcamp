323b1d214795fb1830dd632c836c0409e4ff78c6
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-22 19:41:21 -0700

    adding some structure to the bills table to be shown ehn rendered

--- testing_react/Demo-3-Bills/src/components/BillsTables.jsx
+++ testing_react/Demo-3-Bills/src/components/BillsTables.jsx
@@ -1,5 +1,29 @@
+import React from 'react'
+
 function BillsTable(props) {
-  return <div>BillsTable</div>
+  return (
+    <table className="table w-full">
+      <thead className="text-left">
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Amount</th>
+          <th scope="col">Category</th>
+          <th scope="col" />
+        </tr>
+      </thead>
+      <tbody>
+        {props.bills?.map((value, index) => {
+          return (
+            <tr className="p4" key={index}>
+              <td>{value.date}</td>
+              <td>${value.amount}</td>
+              <td>{value.category}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default BillsTable
