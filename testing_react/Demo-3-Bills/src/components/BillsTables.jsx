14f69b906829c10f29f1454d75a1709917460073
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-22 19:51:04 -0700

    add comments

--- testing_react/Demo-3-Bills/src/components/BillsTables.jsx
+++ testing_react/Demo-3-Bills/src/components/BillsTables.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 function BillsTable(props) {
   return (
     <table className="table w-full">
-      <thead className="text-left">
-        <tr>
-          <th scope="col">Date</th>
-          <th scope="col">Amount</th>
-          <th scope="col">Category</th>
-          <th scope="col" />
+      <thead className="text-left"> {/* used to group table headers */}
+        <tr> {/* table row */}
+          <th scope="col">Date</th> {/* table head */}
+          <th scope="col">Amount</th> {/* table head */}
+          <th scope="col">Category</th> {/* table head */}
+          <th scope="col" /> {/* table head */}
         </tr>
       </thead>
       <tbody>
-        {props.bills?.map((value, index) => {
+        {props.bills?.map((value, index) => { //its something in bills? then map the content and return a table row with the next information on it
           return (
-            <tr className="p4" key={index}>
-              <td>{value.date}</td>
-              <td>${value.amount}</td>
-              <td>{value.category}</td>
+            <tr className="p4" key={index}> {/* this key is unique to each element, bring from index */}
+              <td>{value.date}</td> {/* the date created */}
+              <td>${value.amount}</td> {/* the amount to pay */}
+              <td>{value.category}</td> {/* the category realated */}
             </tr>
           )
         })}
