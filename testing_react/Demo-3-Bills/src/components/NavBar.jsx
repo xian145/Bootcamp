b5a41f6340a941f5641aa8502a02cf4b55355fd9
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-22 21:34:26 -0700

    add som conditions to modified the css when we slect a category and make a new funtion to be called when clicked

--- testing_react/Demo-3-Bills/src/components/NavBar.jsx
+++ testing_react/Demo-3-Bills/src/components/NavBar.jsx
@@ -1,16 +1,27 @@
 function NavBar(props) {
+  const setNewActiveCategory = index => {
+    props.setNewActiveCategory(index)
+  } //this will call the function passed from the parent and send the index of the category
+
+  const liStyle = 'p-4 hover:bg-gray-200 uppercase font-black cursor-pointer'
+  const buttonStyle = 'p-4 inline hover:bg-green-200 uppercase font-black cursor-pointer'
+
   return (
     <ul className='flex justify-center border-b-4 mb-0'> {/* we return a list */}
       {props.categories 
       ? props.categories.map((value, index) => {
         return <li key={index}
-        className='p-4 hover:bg-gray-200 uppercase font-black cursor-pointer'
+        className={liStyle +
+                  (props.activeCategory === index //if the active category is iqual to the index of this category will change the bg color to dark gray
+                    ? ' bg-gray-dark'
+                    : ' bg-gray-lighter')}
+        onClick={() => setNewActiveCategory(value)}
         >{value}</li> //if its something in props.categories gonna make a list item for each value and give a key with the index of the item to each list item
       })
       : '<li>No categories</li>' //if there is nothing in the props.categories gonna make a list item with 'no categories'
       }
       <li onClick={props.showAddCategory}
-      className='p-4 inline hover:bg-green-200 uppercase font-black cursor-pointer'
+      className={buttonStyle}
       >+</li> {/* when pressed add a value in categories array */}
     </ul>
   )
