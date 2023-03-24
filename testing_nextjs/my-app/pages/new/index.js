a596cb201a1068ef503dcc53c775d2bfbf367d43
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-23 22:55:25 -0700

    messing around with URL

---
+++ testing_nextjs/my-app/pages/new/index.js
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>will this work?</h1>
+      <p>and if we call our file 'index.js' in a new folder named 'name' and we put in the URL "localhost:3000/'name'" we render the index.js file</p>
+    </div>
+  )
+}
---
+++ testing_nextjs/my-app/pages/new/newblog.js
@@ -0,0 +1,8 @@
+export default function newBlog() {
+  return (
+    <div>
+      <h1>not actually a blog just messing around</h1>
+      <p>Bassically we create a new folder with some name and we look to that in the URL as we do in a file explorer</p>
+    </div>
+  )
+}
