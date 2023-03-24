9561f4e8fda9e64bf760e23857dd19b8fc6bf0b3
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-03-24 11:31:24 -0700

    in the [id].js file add router to render new pages we look at in URL, or to be called from another file

--- testing_nextjs/my-app/pages/blog/[id].js
+++ testing_nextjs/my-app/pages/blog/[id].js
@@ -1,1 +1,15 @@
 import { useRouter } from 'next/router' //import useRouter so we can handle the dynamic URLs
+
+function blogPost() {
+  const router = useRouter() //to initiate router
+
+  return (
+    <>
+      <h1>Blog post</h1>
+      <p>Post id: {router.query.id}</p>{/* using router we can select using query the id, this will create a new page with the route we use in the URL and use it as 'id' */}
+    {/* as we can saw, there's not a file with the name of the route we search in the navbar, but if we search in the URL a file that actually exist it will rendere it with any issue */}
+    </>
+  )
+}
+
+export default blogPost
