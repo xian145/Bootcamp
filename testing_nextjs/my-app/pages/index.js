7b06c6cb76abf9f1a8164dcbca2c498ea329052f
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-23 23:00:35 -0700

    render the links of the other files in index.js on pages

--- testing_nextjs/my-app/pages/index.js
+++ testing_nextjs/my-app/pages/index.js
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <a href="/blog">Blog</a><br />
+      <a href="/new">New index</a><br />
+      <a href="/new/newblog">New blog</a>
     </div>
   )
 }
