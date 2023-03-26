6741c41e0c99c2731aef2032a78f99ccc87b5ed5
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-03-25 18:44:49 -0700

    added more comments in [id].js to understan how it works

--- testing_nextjs/my-app/pages/blog/[id].js
+++ testing_nextjs/my-app/pages/blog/[id].js
@@ -2,11 +2,13 @@ import { useRouter } from 'next/router' //import useRouter so we can handle the 
 import posts from 'posts.json'
 import DogImage from '../clientdogs'
 
+//this create URLs independent if the URLs actually exists or not, it wont be a 404 error, bc it generates the page always
+
 function blogPost() {
   const router = useRouter() //to initiate router
   const post = posts[router.query.id] //we are looking in the URL for the id, so the user enter that value in the navBar of the browser, after getting the id, the program know what post will render
 
-  if (!post) return <p></p> //thi if avoid an error, bc the when we load the page query start but will be an empty array if the front end is not loaded yet
+  if (!post) return <p>this page dont actually exists but it generate the URL thanks to [id].js</p> //thi if avoid an error, bc the when we load the page query start but will be an empty array if the front end is not loaded yet
 //Now things should work. Initially the component is rendered without the dynamic router.query.id information. 
 //After rendering, Next.js triggers an update with the query value and the page displays the correct information.
 
